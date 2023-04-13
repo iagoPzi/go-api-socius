df0ace8f3a5f5b600f1e502741eec089eda0ff47
Iago Pzivitovski <iagopzivitovski@gmail.com>
2023-04-12 21:25:26 -0300

    string remove

--- main.go
+++ main.go
@@ -11,19 +11,8 @@ import (
 	"github.com/rs/cors"
 )
 
-// func init() {
-// 	chave := make([]byte, 64)
-// 	if _, err := rand.Read(chave); err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	stringBase64 := base64.StdEncoding.EncodeToString(chave)
-// 	fmt.Println(stringBase64)
-// }
-
 func main() {
 	config.Carregar()
-	fmt.Println(config.StringConexaoBanco)
 	fmt.Printf("Listening on port:%d", config.Porta)
 	db.ConnectDB()
 	r := router.Gerar()
